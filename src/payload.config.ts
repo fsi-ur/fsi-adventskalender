c18e42b194be239f293438a1dce6d6ede1b1b827
darius-it <179517889+darius-it@users.noreply.github.com>
2025-11-06 12:27:32 +0100

    feat: temporarily remove s3 integration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -71,20 +71,20 @@ export default buildConfig({
   plugins: [
     ...plugins,
     // storage-adapter-placeholder
-    s3Storage({
-      collections: {
-        media: true,
-      },
-      bucket: process.env.S3_BUCKET || '',
-      config: {
-        credentials: {
-          accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
-          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
-        },
-        region: process.env.S3_REGION || '',
-        // ... Other S3 configuration
-      },
-    }),
+    // s3Storage({
+    //   collections: {
+    //     media: true,
+    //   },
+    //   bucket: process.env.S3_BUCKET || '',
+    //   config: {
+    //     credentials: {
+    //       accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+    //       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+    //     },
+    //     region: process.env.S3_REGION || '',
+    //     // ... Other S3 configuration
+    //   },
+    // }),
   ],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
